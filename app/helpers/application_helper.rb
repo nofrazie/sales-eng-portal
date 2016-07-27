fe6f8c999359be7e0e0f2118f3a8a639f2c8f508
Nolan Frazier <nofrazier3@gmail.com>
2016-07-27 14:39:15 -0400

    Working on login functionality for SE app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Procore - Sales Engineering App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
