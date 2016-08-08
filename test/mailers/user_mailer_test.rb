59c2c643883c4fc51e44d2dfb68023c5789de2a7
Nolan Frazier <nofrazier3@gmail.com>
2016-08-07 19:37:13 -0700

    Fixed activation emails

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-
   test "account_activation" do
     mail = UserMailer.account_activation
     assert_equal "Account activation", mail.subject
@@ -17,4 +16,5 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ["from@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
+
 end
