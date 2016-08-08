ce3683e2e096a5c15f1e84da429dfcf1807b76fb
Nolan Frazier <nofrazier3@gmail.com>
2016-08-07 21:52:22 -0700

    added signin and signup links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root    'sessions#new'
+  root    'static_pages#home'
   get     '/help',    to: 'static_pages#help'
   get     '/signup',  to: 'users#new'
   post    '/signup',  to: 'users#create'
