1466a36d070ca430ed696346261bde40d1528c1a
Nolan Frazier <nofrazier3@gmail.com>
2016-08-24 10:29:06 -0700

    Added new action in integration_types controller along with view

--- app/controllers/integration_types_controller.rb
+++ app/controllers/integration_types_controller.rb
@@ -1,9 +1,24 @@
 class IntegrationTypesController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   def index
     @integration_types = IntegrationType.all
   end
 
   def show
-    @integration_type = Integration.find(param[:id])
+    @integration_type = IntegrationType.find(param[:id])
+  end
+
+  def new
+    @integration_type = IntegrationType.new
+  end
+
+  def create
+    @integration_type = IntegrationType.new(integration_type_params)
+    if @integration_type.save
+      redirect_to integration_types
+    else
+      render 'new'
+    end
   end
 end
