417f39449b1f9c0a0e60140282ed9ff4310e920e
Nolan Frazier <nofrazier3@gmail.com>
2016-12-01 23:50:51 -0800

    Added seeds to file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,5 @@ LearningResourceType.create(name: "Article")
 LearningResourceType.create(name: "Book")
 LearningResourceType.create(name: "Podcast")
 LearningResourceType.create(name: "Video")
+
+LearningResource.create(title: "The Evolution of Sales Engineering", link: "https://www.linkedin.com/pulse/evolution-sales-engineering-organization-jonathan-michaels", learning_resource_type_id: 1, user_id: 1)
